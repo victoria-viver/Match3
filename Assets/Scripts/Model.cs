142aecb5410e8db1e5d5066a676585cc2ce3af27
Victoria Shenkevich <Victoria.Shenk@gmail.com>
2019-12-17 04:04:00 +0200

    Score counting is added

--- Assets/Scripts/Model.cs
+++ Assets/Scripts/Model.cs
@@ -1,17 +1,20 @@
-﻿using System; 
-using UnityEngine;
-/**
+﻿/**
 * Created by: Victoria Shenkevich
 * Created on: 15/12/2019
 */
+
+using System; 
+using UnityEngine;
 
 public static class Model 
 {
     public const int BORDER_GAP = 100;
     public const float ITEM_SCALE = 0.9f;
 
-    public const int ROWS = 7;
+    public const int ROWS = 5;
     public const int COLS = 7;
+
+    public const int POINTS_PER_ITEM = 10;
 
     public static int CellSize 
     { 
