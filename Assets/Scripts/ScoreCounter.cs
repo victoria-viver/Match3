d196541101e92f49c5a9bc9c8d1f5c76226224eb
Victoria Shenkevich <Victoria.Shenk@gmail.com>
2019-12-17 06:57:03 +0200

    Score logic is separated to independent class

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,56 @@
+﻿/**
+* Created by: Victoria Shenkevich
+* Created on: 15/12/2019
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    #region Private Fields
+	private int currentScore = 0;
+	private int targetScore = 0;
+
+    private int animLengthInFrames = 20;
+    private int currentFrame = 0;
+    #endregion
+
+
+    #region Links
+    [SerializeField] private Text score;
+    #endregion
+
+
+    #region Unity Methods
+    void Awake ()
+    {
+        score = GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        score.text = string.Empty;
+    }
+
+    void Update()
+    {
+        if (currentScore < targetScore)
+        {
+            currentFrame++;
+            currentScore += (targetScore - currentScore) * currentFrame / animLengthInFrames;
+            currentFrame = (currentFrame == animLengthInFrames) ? 0 : currentFrame;
+
+            score.text = currentScore.ToString();
+        }
+    }
+    #endregion
+
+
+    #region Public Methods
+    public void UpdateScore (int newPoints)
+    {
+        targetScore += newPoints;
+    }
+    #endregion
+}
